3b67581ebafdc2430331a422d81b30968784a910
strenev <stan@0xfusion.com>
2023-10-18 13:53:04 +0300

    feat: add opengraph data

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,18 +10,14 @@ import { CoingeckoContextWrapper } from "@/context/coingecko.context";
 
 const config = createConfig(
   getDefaultConfig({
-    // Required API Keys
-    alchemyId: process.env.ALCHEMY_API_KEY, // or infuraId
+    alchemyId: process.env.ALCHEMY_API_KEY,
     walletConnectProjectId: process.env.WALLET_CONNECT_PROJECT_ID || "",
-
-    // Required
     appName: "ApeBlendr",
-
-    // Optional
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
-    chains: [goerli]
+    appDescription:
+      "Enter the $APE Blendr and win prizes every week. No one loses ever.",
+    appUrl: "https://apeblendr.com",
+    appIcon: "https://apeblendr.com/logo.png",
+    chains: [goerli],
   })
 );
 
@@ -33,31 +29,73 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>
+          ApeBlendr - Enter the $APE Blendr and win prizes every week. No one
+          loses ever.
+        </title>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
+        <meta charSet="utf-8"></meta>
+        <meta name="robots" content="index,follow"></meta>
+        <meta
+          name="description"
+          content="ApeBlendr is a no-loss savings game inspired by PoolTogether and built on top of the ApeCoin protocol. Currently, ApeCoin provides a staking program with juicy yields, so why not gamify it?"
+        ></meta>
+        <meta
+          property="og:title"
+          content="ApeBlendr - Enter the $APE Blendr and win prizes every week. No one loses ever."
+        ></meta>
+        <meta
+          property="og:description"
+          content="ApeBlendr is a no-loss savings game inspired by PoolTogether and built on top of the ApeCoin protocol. Currently, ApeCoin provides a staking program with juicy yields, so why not gamify it?"
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta
+          property="og:image"
+          content="https://apeblendr.com/preview.png"
+        ></meta>
+        <meta
+          property="og:image:alt"
+          content="ApeBlendr - Enter the $APE Blendr and win prizes every week. No one loses ever."
+        ></meta>
+        <meta property="og:image:width" content="1200"></meta>
+        <meta property="og:image:height" content="627"></meta>
+        <meta name="viewport" content="width=device-width"></meta>
       </head>
       <WagmiConfig config={config}>
-        <ConnectKitProvider mode="light" customTheme={{
-          "--ck-font-family": '"Space Mono", monospace',
-          "--ck-connectbutton-color": "#fff",
-          "--ck-connectbutton-hover-color": "#fff",
-          "--ck-connectbutton-background": "rgb(0, 25, 234)",
-          "--ck-connectbutton-active-color": "#fff",
-          "--ck-connectbutton-active-background": "rgb(0, 25, 234)",
-          "--ck-connectbutton-hover-background": "#0b1db5",
-          "--ck-connectbutton-border-radius": "4px"
-        }}>
+        <ConnectKitProvider
+          mode="light"
+          customTheme={{
+            "--ck-font-family": '"Space Mono", monospace',
+            "--ck-connectbutton-color": "#fff",
+            "--ck-connectbutton-hover-color": "#fff",
+            "--ck-connectbutton-background": "rgb(0, 25, 234)",
+            "--ck-connectbutton-active-color": "#fff",
+            "--ck-connectbutton-active-background": "rgb(0, 25, 234)",
+            "--ck-connectbutton-hover-background": "#0b1db5",
+            "--ck-connectbutton-border-radius": "4px",
+          }}
+        >
           <AlchemyContextWrapper>
             <SubgraphContextWrapper>
               <CoingeckoContextWrapper>
-              <body>
-                <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-                  <Navbar />
-                  <div style={{ flexGrow: 1 }}>{children}</div>
-                  <Footer />
-                </div>
-              </body>
+                <body>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      minHeight: "100vh",
+                    }}
+                  >
+                    <Navbar />
+                    <div style={{ flexGrow: 1 }}>{children}</div>
+                    <Footer />
+                  </div>
+                </body>
               </CoingeckoContextWrapper>
             </SubgraphContextWrapper>
           </AlchemyContextWrapper>
